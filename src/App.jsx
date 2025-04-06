9acf4a7b5febdd34ab213f238f6eaf0fe8525c0a
parikshitbarua <parikshitb04@gmail.com>
2025-04-06 23:00:40 +0530

    redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
+import { Provider } from "react-redux";
 import Header from "./components/Header.jsx";
 import Body from "./components/Body.jsx";
 import Error from "./pages/Error.jsx";
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
 import RestaurantMenu from "./pages/RestaurantMenu.jsx";
+import appStore from "./utils/store/appStore.jsx";
 
 // lazy load the Grocery module.
 const Grocery = lazy(() => import("./components/Grocery.jsx"));
 
 const AppLayout = () => {
     return (
-        <div className="app">
-            <Header />
-            <Outlet />
-        </div>
+        <Provider store={appStore}>
+            <div className="app">
+                <Header />
+                <Outlet />
+            </div>
+        </Provider>
     )
 };
 
--- src/components/MenuCategoryItem.jsx
+++ src/components/MenuCategoryItem.jsx
@@ -1,12 +1,25 @@
+import { useDispatch } from "react-redux";
+import { addItem } from "../utils/store/cartSlice.jsx";
 import { CDN_URL } from "../utils/constants.jsx";
 
 const MenuCategoryItem = ({ menuItem }) => {
+    const dispatch = useDispatch();
+    const handleAddItem = () => {
+        dispatch(addItem("pizza"));
+    }
     return (
         <div className="menu-item">
             <div>
                 <h3>{menuItem?.card?.info?.name}</h3>
                 <p>{menuItem?.card?.info?.description}</p>
-                <p>₹ {menuItem?.card?.info?.price / 100 || menuItem?.card?.info?.defaultPrice / 100}</p>
+                <p>
+                    <span>
+                        ₹ {menuItem?.card?.info?.price / 100 || menuItem?.card?.info?.defaultPrice / 100}
+                    </span>
+                    <span>
+                    <button className="add-item-button" onClick={handleAddItem}>+ Add</button>
+                    </span>
+                </p>
             </div>
             <div className="item-image">
                 <img
